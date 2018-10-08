9f82a431a0d5ce7a3ae19ff353e01bde7d98a414
jag <jagnoor@gmail.com>
2018-10-08 14:41:31 -0500

    commit from work

--- config/keys.js
+++ config/keys.js
@@ -2,4 +2,3 @@ module.exports = {
 mongoURI: 'mongodb://jagnoor:jag12345@ds125293.mlab.com:25293/jag-devconnector',
 secretorKey: 'secret'
 };
-
--- routes/api/users.js
+++ routes/api/users.js
@@ -93,7 +93,7 @@ router.post('/login', (req, res) => {
         jwt.sign(
           payload,
           keys.secretOrKey,
-          { expiresIn: 33600 },
+          { expiresIn: 3600 },
           (err, token) => {
             res.json({
               success: true,
